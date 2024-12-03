a663106ba6b95e621f410fb1468bc1cba04340f7
Anurag Dhadse <hello@adhadse.com>
2024-12-03 10:02:37 +0530

    ensurepip before

--- setup_ci.py
+++ setup_ci.py
@@ -10,11 +10,13 @@ import setuptools
 from setuptools.command.build_ext import build_ext
 
 print("Python path before:", sys.path)
+import ensurepip
+ensurepip.bootstrap()
+
 subprocess.check_call([sys.executable, "-m", "pip", "install", "pybindgen"])
 print("Python path after:", sys.path)
 
-import ensurepip
-ensurepip.bootstrap()
+
 
 version = os.environ.get('RELEASE_VERSION', None)
 if version is None:
